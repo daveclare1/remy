01e78690a8dd3632fc4bac7914bb177ac91d80fd
daveclare1 <31649171+daveclare1@users.noreply.github.com>
2024-07-02 09:11:20 +0000

    Created initial structure from tutorial

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,11 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read(
+    worksheet="tracker",
+    ttl=60,
+    )
+
+st.dataframe(df)
