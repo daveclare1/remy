2ac87e625eb0735d2db7ce95c4e375329972d419
daveclare1 <31649171+daveclare1@users.noreply.github.com>
2024-07-03 08:10:15 +0000

    nicely styled plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,15 @@
+import numpy as np
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
-import plotly.express as px
+import plotly.graph_objects as go
+
+marker_styles = {
+    'drink': dict(symbol='line-ns', size=10, line_width=2, color='lightskyblue'),
+    'food': dict(symbol='line-ns', size=20, line_width=2, color='black'),
+    'poo': dict(symbol='triangle-up', color='brown', opacity=0.7),
+    'wee': dict(symbol='circle', color='lightskyblue', opacity=0.7),
+}
 
 conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read(
@@ -12,9 +20,8 @@ df = df.convert_dtypes()
 
 df['timestamp'] = df['date'] + " " + df['time']
 df.timestamp = pd.to_datetime(df.timestamp, dayfirst=True)
-# df = df.drop(['date', 'time'], axis='columns')
 df.date = pd.to_datetime(df.date, dayfirst=True)
-df.time = pd.to_datetime(df.time)
+df.time = pd.to_datetime(df.time, format="%H:%M:%S")
 
 # Now we have a clean dataframe
 
@@ -57,24 +64,27 @@ df['poo_num'] = col_poo_num
 df['day_idx'] = df.groupby('date').ngroup()
 df['hit'] = (df['where'] == 'grass') | (df['where'] == None)
 
+df['y_scatter'] = np.random.randint(-3600, 3600, df.shape[0])
+df['y_scatter'] = df['y_scatter'].apply(lambda s:pd.Timedelta(seconds=s))
+df['y_scatter'] = df['y_scatter'].mask(df.action.isin(['food','drink']), pd.Timedelta(0))
+
 # plot timelines
 
-fig = px.scatter(
-    df,
-    x='time',
-    y='date',
-    color='action',
-    symbol = 'action',
-    symbol_sequence= ['circle', 'square', 'diamond-tall', 'hourglass'],
-)
-fig.update_traces(marker=dict(size=12, opacity=0.7))
+fig = go.Figure()
+
+for action, df_action in df.groupby('action'):
+    fig.add_scatter(
+        x=df_action.time,
+        y=df_action.date + df_action.y_scatter,
+        mode='markers',
+        showlegend=True,
+        name=action,
+        marker=marker_styles[action],
+    )
+
 fig.update_layout(xaxis_tickformat='%H:%M')
 fig.update_xaxes(nticks=10, showgrid=True)
 fig.update_yaxes(nticks=int(df.day_idx.max())+1, showgrid=True)
 st.plotly_chart(fig)
-
-for date, df_date in df.groupby('date'):
-    print(date)
-
 
 st.dataframe(df)
